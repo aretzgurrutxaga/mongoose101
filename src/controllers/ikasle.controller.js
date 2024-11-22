2ca786ed7ca0bbdc443a18dc63c77a8ab01cef0f
Aretz Gurruchaga Ayerza <120658503+aretzgurrutxaga@users.noreply.github.com>
2024-11-22 14:39:23 +0000

    edit falta

--- src/controllers/ikasle.controller.js
+++ src/controllers/ikasle.controller.js
@@ -31,8 +31,28 @@ exports.getIkasleById = async (req, res, next) => {
     }
 };
 
-exports.deleteIkasle = async (req ,res ,next) => {
-    
+exports.deleteIkasleById = async (req, res, next) => {
+    try {
+        const ikasle = await Ikasle.findByIdAndDelete(req.params.id);
+        if (!ikasle) {
+            return res.status(404).json({ message: 'Ikaslea ez da aurkitu' });
+        }
+        res.json(ikasle);
+    } catch (error) {
+        next(error);
+    }
 }
+
+exports.updateIkasleById = async (req, res, next) => {
+    try {
+        const ikasle = await Ikasle.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!ikasle) {
+            return res.status(404).json({ message: 'Ikaslea ez da aurkitu' });
+        }
+        res.json(ikasle);
+    } catch (error) {
+        next(error);
+    }
+};
 
 // Gehitu beste kontroladoreak...
